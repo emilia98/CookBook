fe8b5bc511adaf4762836f0b19ba61ac167850fd
emilia98 <emilia_nedialkova@abv.bg>
2021-12-28 18:54:01 +0200

    add Data project

---
+++ CookBook/Data/CookBook.Data/Seeding/UsersSeeder.cs
@@ -0,0 +1,42 @@
+﻿using CookBook.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookBook.Data.Seeding
+{
+    internal class UsersSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            await SeedUserAsync(userManager, "admin", "admin@cookbook.com", "admin1234");
+            await SeedUserAsync(userManager, "editor", "editor@cookbook.com", "editor1234");
+            await SeedUserAsync(userManager, "ordinary_user", "ordinary_user@cookbook.com", "user1234");
+        }
+
+        private static async Task SeedUserAsync(UserManager<ApplicationUser> userManager, string username, string email, string password)
+        {
+            var user = await userManager.FindByNameAsync(username);
+
+            if (user == null) 
+            {
+                var result = await userManager.CreateAsync(
+                    new ApplicationUser() {
+                        UserName = username,
+                        Email = email,
+                        NormalizedUserName = username.ToUpper(),
+                        NormalizedEmail = email.ToUpper()
+                    }, password);
+
+                if(!result.Succeeded) 
+                {
+                    throw new Exception(string.Join(Environment.NewLine, result.Errors.Select(e => e.Description)));
+                }
+            }
+        }
+    }
+}
---
+++ CookBook/Data/CookBook.Data/Seeding/UsersToRolesSeeder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook.Data.Seeding
+{
+    internal class UsersToRolesSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
