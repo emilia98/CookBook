bd59298ff7a44a417962e6691ab1df98fa9aa9dc
emilia98 <emilia_nedialkova@abv.bg>
2021-12-28 18:20:51 +0200

    add Data Models project

---
+++ CookBook/Data/CookBook.Data.Models/ApplicationRole.cs
@@ -0,0 +1,27 @@
+﻿using CookBook.Data.Common.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace CookBook.Data.Models
+{
+    public class ApplicationRole : IdentityRole<int>, IAuditInfo, IDeletableEntity
+    {
+        public ApplicationRole() 
+            : this(null)
+        {
+        }
+
+        public ApplicationRole(string name)
+            : base(name)
+        {
+        }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+    }
+}
